1be569a4ed90ebe2e46c8503fd9c885e6490b509
apdRV <apodmarev@list.ru>
2024-09-10 23:50:32 +0300

    added second task and tests

--- task-2/run-autotests.py
+++ task-2/run-autotests.py
@@ -1,1 +1,19 @@
 from task2 import nfa_to_dfa
+
+# TEST 1
+nfa_to_dfa("tests/test1")
+t1 = open('tests/test1-ans.txt').read()
+t2 = open('tests/test1.sol').read()
+assert(t1 == t2)
+
+# TEST 2
+nfa_to_dfa("tests/test2")
+t1 = open('tests/test2-ans.txt').read()
+t2 = open('tests/test2.sol').read()
+assert(t1 == t2)
+
+# TEST 3
+nfa_to_dfa("tests/test3")
+t1 = open('tests/test3-ans.txt').read()
+t2 = open('tests/test3.sol').read()
+assert(t1 == t2)
